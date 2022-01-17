ca6de63a185bde06d8aa2b795ea02aa56674472d
theblindhobo <theblindhobo@gmail.com>
2022-01-16 19:35:29 -0700

    Upgrade and fix bugs

---
+++ README.md
@@ -0,0 +1,21 @@
+## Scheduler
+
+*Grabs information from Google Sheet and schedules jobs based on date/time. These jobs always write to a log file (that is used to Switch Scenes in OBS) and they may send Lower Third title information to another application.*
+
+
+---
+**Setup:**
+1.  Download repo
+2.  Open terminal in folder
+3.  run `npm i` to install all the necessary modules
+4.  Websocket port used is set to **3124**, if you're wanting to change this port.. you must do so within the **scheduler.js** file on lines 15 & 58.
+5.  You need a Google Cloud Platform account setup with Google Sheet API, API Key, Client ID, Client Secret.
+6.  In the **.env** file, add `SPREADSHEET_ID=""` with the ID of the spreadsheet you'll be reading from.
+7.  If your token isn't working, delete the **token.json** file and run the app `node .`. Follow the directions it gives you.
+
+---
+**Running:**
+1.  Open a terminal in this folder
+2.  Run process `node .`
+3.  Leave this process running so scheduler can schedule jobs and talk freely with the websocket server.
+4.  Logs to **log.txt** within this same folder.
--- scheduler.js
+++ scheduler.js
@@ -8,6 +8,61 @@ dotenv.config();
 
 const refresh = 10000; // milliseconds
 
+
+const WebSocket = require('ws');
+var client;
+function connectWebsocket() {
+  client = new WebSocket('ws://localhost:3124');
+  client.on('open', function open() {
+    console.log(`\x1b[35m%s\x1b[0m`, `[WEBSOCKET]`, `Connected at ws://localhost:3124`);
+  });
+  function heartbeat(client) {
+    clearTimeout(client.pingTimeout);
+    client.pingTimeout = setTimeout(() => {
+      client.close(1000, 'Terminated');
+    }, 30000 + 1000);
+  }
+  client.on('message', async function(event) {
+    const data = JSON.parse(event);
+    if(data.event === 'ping') {
+      heartbeat(client);
+      client.send(JSON.stringify({ "event": "pong" }));
+    }
+  });
+
+  client.on('error', (err) => {
+    // console.log(`\x1b[32m%s\x1b[0m`, `[WEBSOCKET]`, `Error:`, err.errno);
+  });
+  client.on('close', function(event) {
+    clearTimeout(client.pingTimeout);
+    client = null; // connection died, discard old and create new
+    console.log(`\x1b[35m%s\x1b[31m%s\x1b[33m%s\x1b[31m%s\x1b[0m`, `[WEBSOCKET]`, ` Error: `, event, `\t Disconnected from WebSocket..`, `reconnecting..`);
+    setTimeout(function () {
+      connectWebsocket(); // Reconnect
+    }, 10000);
+  });
+  return client;
+}
+connectWebsocket();
+var ws;
+const wsOpenListener = (event) => {
+  console.log(`\x1b[35m%s\x1b[0m`, `[WEBSOCKET]`, `Connected.`);
+};
+const wsMessageListener = (event) => {
+  console.log(`\x1b[35m%s\x1b[0m`, `[WEBSOCKET]`, `Received message.`);
+};
+const wsCloseListener = (event) => {
+  if(ws) {
+    console.log(`\x1b[35m%s\x1b[0m`, `[WEBSOCKET]`, `Disconnected.`);
+  }
+  ws = new WebSocket('ws://localhost:3124');
+  ws.on('open', wsOpenListener);
+  ws.on('message', wsMessageListener);
+  ws.on('close', wsCloseListener);
+};
+
+
+
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 const TOKEN_PATH = 'token.json';
@@ -61,7 +116,7 @@ function searchSheet(auth) {
   const sheets = google.sheets({version: 'v4', auth});
   sheets.spreadsheets.values.get({
     spreadsheetId: process.env.SPREADSHEET_ID,
-    range: 'SCHEDULE!A2:D',
+    range: 'MASTER SCHEDULE!A2:K',
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
     const rows = res.data.values;
@@ -72,46 +127,75 @@ function searchSheet(auth) {
           var datum = Date.parse(strDate);
           return datum/1000;
         }
-        let timestamp = toTimestamp(`${row[0]} ${row[1]}`); // formats based on Timezone added on sheet
-        let date = new Date(timestamp * 1000);
+        // Checks if there is an ACTION & FILE/ETC
+        if(row.length >= 4) {
+          let rowObj = {
+            datetime: row[0],
+            timezone: row[1] ? row[1] : 'UTC',
+            action: row[2],
+            uri: row[2] == 'LIVE' ? row[3] : '',
+            name: row[4],
+            line1: row[5] ? row[5] : '',
+            line2: row[6] ? row[6] : ''
+          };
 
-        let now = Date.parse(new Date);
+          if(rowObj.action == '' || rowObj.name == '') {
+            // partial row
+          } else {
+            let timestamp = toTimestamp(`${rowObj.datetime} ${rowObj.timezone}`); // formats based on Timezone added on sheet
+            let date = new Date(timestamp * 1000);
 
+            let now = Date.parse(new Date);
 
-        if(timestamp * 1000 < now) {
-          // don't schedule job, time has passed already
-        } else {
-          jobs.push(timestamp);
+            if(timestamp * 1000 < now) {
+              // don't schedule job, time has passed already
+            } else {
+              jobs.push(timestamp);
 
-          let myObject = [schedule.scheduledJobs];
-          if(myObject.find(e => e[timestamp.toString()])) {
-            // cancel and reschedule
-            var myJob = schedule.scheduledJobs[timestamp.toString()];
-            myJob.cancel();
-            const index = jobs.indexOf(timestamp);
-            if(index > -1) {
-              jobs.splice(index, 1);
-            }
-            // re-add to array for comparison
-            jobs.push(timestamp);
+              let myObject = [schedule.scheduledJobs];
+              if(myObject.find(e => e[timestamp.toString()])) {
+                // cancel and reschedule
+                var myJob = schedule.scheduledJobs[timestamp.toString()];
+                myJob.cancel();
+                const index = jobs.indexOf(timestamp);
+                if(index > -1) {
+                  jobs.splice(index, 1);
+                }
+                // re-add to array for comparison
+                jobs.push(timestamp);
 
-          }
-          // schedule job
-          var j = schedule.scheduleJob(`${timestamp}`,date, function(){
-            console.log(row[0]);
-
-            fs.writeFile('log.txt', `${row[2]}:${row[3]}`, err => {
-              if(err) {
-                console.log(err);
-                return
               }
-              console.log(`File written successfully as: ${row[2]}:${row[3]}`);
-            });
+              // schedule job
+              var j = schedule.scheduleJob(`${timestamp}`,date, function(){
 
-          });
-
+                if(rowObj.action == 'LIVE') {
+                  let socket = client ? client : 'closed';
+                  if(socket == 'closed') {
+                    console.log(`\x1b[35m%s\x1b[0m`, `[WEBSOCKET]`, `Cannot send titles because websocket is closed.`);
+                  } else {
+                    socket.send(JSON.stringify({
+                        "event": "titles",
+                        "data": {
+                          "action": rowObj.action,
+                          "name": rowObj.name,
+                          "line1": rowObj.line1,
+                          "line2": rowObj.line2
+                        }
+                    }));
+                  }
+                }
+                let content = (rowObj.action == 'LIVE') ? `${rowObj.uri}:${rowObj.name}` : `${rowObj.action}:${rowObj.name}`;
+                fs.writeFile('log.txt', content, err => {
+                  if(err) {
+                    console.log(`\x1b[31m%s\x1b[0m`, `[SCHEDULER]`, err);
+                    return
+                  }
+                  console.log(`\x1b[36m%s\x1b[0m%s\x1b[33m%s\x1b[0m`, `\n[SCHEDULER]`, ` Successfully logged to file as: ${content}`, `\t ${rowObj.datetime} ${rowObj.timezone}\n`);
+                });
+              });
+            }
+          }
         }
-
       });
 
       if(Object.keys(schedule.scheduledJobs).length !== jobs.length) {
@@ -122,14 +206,13 @@ function searchSheet(auth) {
           myJob.cancel();
         }
       }
-      console.log(`Number of scheduled jobs: ${Object.keys(schedule.scheduledJobs).length}`);
     } else {
-      console.log('No data found.');
+      console.log(`\x1b[33m%s\x1b[0m`, `[SCHEDULER]`, `\x1b[33m%s\x1b[0m`, 'No data found.');
     }
   });
 
   setTimeout(() => {
-    console.log(`refreshing... `);
+    console.log(`\x1b[36m%s\x1b[0m%s\x1b[33m%s\x1b[32m%s\x1b[0m`, `[SCHEDULER]`, `\t Scheduled jobs: `, Object.keys(schedule.scheduledJobs).length, `\t refreshing... `);
     searchSheet(auth);
   }, refresh);
 }
